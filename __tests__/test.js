13e55eb8928df43cca8e1d275ff8ee591dd8cc86
Zunayed Mahfuz <zunayed.eshna@gmail.com>
2020-08-25 17:50:00 -0400

    wop

--- __tests__/test.js
+++ __tests__/test.js
@@ -5,3 +5,5 @@ let algos = require("../largestOfEach.js");
 test("returns an array of the largest integer in each array", () => {
   expect(algos.largestOfEach([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]])).toMatchObject([5, 27, 39, 1001]);
 });
+
+// wop
